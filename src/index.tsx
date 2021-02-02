4501a4ff16ead0eae8d22e1e535a204b15ce13a9
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-02 10:02:02 +0200

    Sample: added a sample usecase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {ReactUIBucketProvider, theme} from './react-ui-bucket';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactUIBucketProvider theme={theme.pinkHigh}>
+      <App />
+    </ReactUIBucketProvider>
   </React.StrictMode>,
   document.getElementById('app-root')
 );
--- src/lib/components/lib/theme-provider/theme-provider.lib.tsx
+++ src/lib/components/lib/theme-provider/theme-provider.lib.tsx
@@ -7,7 +7,9 @@ type TReactUIBucketProvider = {
 
 /************************************
  * @ReactUIBucketProvider for Create-react-app users wrap it around your App component in the index.js|ts file. for other bootstrapers, wrap it around your root file that renders to the DOM elementNode
- ************************************/
+ * @useCase to use our custom theme, simply import it from react-ui-buclet together with this component e.g: import {ReactUIBucketProvider, theme} from './react-ui-bucket';
+ * ***********************************/
+
 export const ReactUIBucketProvider: React.FC<TReactUIBucketProvider> = ({children, theme}): JSX.Element => {
   return (
     <div
