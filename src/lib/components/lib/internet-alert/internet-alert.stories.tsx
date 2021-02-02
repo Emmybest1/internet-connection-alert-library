87fe70f20d0f3724b663f9ebcf78d0150d98fca5
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-02 10:57:36 +0200

    added more prop value

--- src/lib/components/lib/internet-alert/internet-alert.stories.tsx
+++ src/lib/components/lib/internet-alert/internet-alert.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {InternetAlert} from './internet-alert.lib';
-import {storiesHierarchy as h} from '../../../../react-ui-bucket';
+import {storiesHierarchy as h, theme} from '../../../../react-ui-bucket';
 
 export default {
   title: `${h.hPages}/InternetAlert View`,
   component: InternetAlert,
 };
 
-export const InternetAlertView = () => <InternetAlert bodyText="Opps internet is broken" />;
+export const InternetAlertView = () => (
+  <InternetAlert bodyText="Opps internet is broken" backgroundColor={theme.pinkLow} />
+);
