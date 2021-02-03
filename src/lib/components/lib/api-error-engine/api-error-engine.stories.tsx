0e49ee5a85299a56c03150f99fe693a9282730b4
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-03 07:13:41 +0200

    Feature: added a story

--- src/lib/components/lib/api-error-engine/api-error-engine.stories.tsx
+++ src/lib/components/lib/api-error-engine/api-error-engine.stories.tsx
@@ -1,1 +1,20 @@
-export {}
+import React from 'react';
+import {ApiErrorEngine} from './api-error-engine.lib';
+import {storiesHierarchy as h, _truthyOrFalsyDetector} from '../../../../react-ui-bucket';
+
+export default {
+  title: `${h.hPages}/ApiErrorEngine View`,
+  component: ApiErrorEngine,
+};
+
+export const DefaultHeadingApiErrorEngine = () => (
+  <ApiErrorEngine shouldShowModal={_truthyOrFalsyDetector()} message="Unathorized User is not allowed" />
+);
+
+export const customHeadingApiErrorEngine = () => (
+  <ApiErrorEngine
+    shouldShowModal={_truthyOrFalsyDetector()}
+    heading="API Auth Error"
+    message="Unathorized User is not allowed"
+  />
+);
