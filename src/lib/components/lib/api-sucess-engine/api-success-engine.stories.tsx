c1811de42cb6aed1e7c9724237bc1ce4b737dd9c
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-03 07:17:14 +0200

    Feature: added a story

--- src/lib/components/lib/api-sucess-engine/api-success-engine.stories.tsx
+++ src/lib/components/lib/api-sucess-engine/api-success-engine.stories.tsx
@@ -1,1 +1,20 @@
-export {};
+import React from 'react';
+import {ApiSuccessEngine} from './api-success-engine.lib';
+import {storiesHierarchy as h, _truthyOrFalsyDetector} from '../../../../react-ui-bucket';
+
+export default {
+  title: `${h.hPages}/ApiSuccessEngine View`,
+  component: ApiSuccessEngine,
+};
+
+export const DefaultHeadingApiSuccessEngine = () => (
+  <ApiSuccessEngine shouldShowModal={_truthyOrFalsyDetector()} message="Success Authorized" />
+);
+
+export const customHeadingApiErrorEngine = () => (
+  <ApiSuccessEngine
+    shouldShowModal={_truthyOrFalsyDetector()}
+    heading="API Success"
+    message="Unathorized User is not allowed"
+  />
+);
