7eaabaa24bf782bbbe1f627e53bfee1cc9fefd41
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-18 01:58:36 +0200

    Update

--- src/lib/hooks/use-localstorage/use-localstorage.lib.ts
+++ src/lib/hooks/use-localstorage/use-localstorage.lib.ts
@@ -1,23 +1,32 @@
-import {useEffect} from 'react';
+import {MutableRefObject, useEffect, useRef} from 'react';
 
 type TUseLocalStorageProps = {
   key: string;
   value: any;
   invoker?: any;
 };
+
 export const useLocalstorage = ({key, value, invoker}: TUseLocalStorageProps) => {
+  let isMounted: MutableRefObject<null | boolean> = useRef<null | boolean>(null);
+
   useEffect(() => {
-    switch (value) {
-      case typeof value !== 'object':
-        window.localStorage.setItem(key, value);
-        break;
+    isMounted.current = true;
+    if (isMounted) {
+      switch (value) {
+        case typeof value !== 'object':
+          window.localStorage.setItem(key, value);
+          break;
 
-      case typeof value === 'object' && !Array.isArray(value):
-        window.localStorage.setItem(key, JSON.stringify(value));
-        break;
+        case typeof value === 'object' && !Array.isArray(value):
+          window.localStorage.setItem(key, JSON.stringify(value));
+          break;
 
-      default:
-        window.localStorage.setItem(key, value);
+        default:
+          window.localStorage.setItem(key, value);
+      }
     }
+    return () => {
+      isMounted.current = false;
+    };
   }, [invoker ?? undefined]);
 };
