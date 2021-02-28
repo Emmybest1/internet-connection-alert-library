e84345fb10c2b5a4f9e3b4ce38b421c7abca58c2
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-28 05:33:01 +0200

    Update: type=hook

--- src/hooks/useUniqueIds.ts
+++ src/hooks/useUniqueIds.ts
@@ -1,4 +1,4 @@
 import {useRef} from 'react';
 import uniqid from 'uniqid';
 
-export const useUniqueIds = (count: number) => useRef([...new Array(count)].map(() => uniqid())).current;
+export const useUniqueIds = (count: number) => useRef([...new Array(Math.floor(count))].map(() => uniqid())).current;
