000cf06c1615473554180d220cf55814f38e628c
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-28 06:42:53 +0200

    Feature: type=test

--- src/libraries/utils/decode/decode.lib.ts
+++ src/libraries/utils/decode/decode.lib.ts
@@ -2,7 +2,4 @@
  * @decode you can use this function to decode an encoded uri & strings
  * usecase:decode("%20%DD%40%33")
  *************/
-
-export const decode: (arg: string) => string = (value: string): string => {
-  return window.decodeURIComponent(value);
-};
+export const decode = (arg: string): string => window.decodeURIComponent(arg);
--- src/libraries/utils/decode/decode.test.ts
+++ src/libraries/utils/decode/decode.test.ts
@@ -1,8 +1,7 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {decode} from './decode.lib';
 
 describe('Test for decode function', () => {
-  test('should...', () => {
-    expect(2 + 2).toBe(4);
+  test('should decode and pass', () => {
+    expect(decode('http%3A%2F%2Fwww.mysite.com%2Fa+file+with+spaces.html')).not.toContain('%');
   });
 });
