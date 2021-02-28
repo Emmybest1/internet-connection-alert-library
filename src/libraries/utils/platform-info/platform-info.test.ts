3265e5b75b3f729402e27ebb7b2989af0868f5dd
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-28 06:29:49 +0200

    Feature: type=test

--- src/libraries/utils/platform-info/platform-info.test.ts
+++ src/libraries/utils/platform-info/platform-info.test.ts
@@ -1,8 +1,13 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {system, browser} from './platform-info.lib';
 
-describe('Test for ... function', () => {
-  test('should...', () => {
-    expect(2 + 2).toBe(4);
+describe('Test for platform-info.test.ts', () => {
+  test('should check if browser is firfox and version defined', () => {
+    expect(browser.name).toContain('Firefox');
+    expect(browser.version).toBeDefined();
+  });
+
+  test('should pass if system os is undefined because this is runnit in headless mode and can only detect system os with the help of some realtime browser helpers', () => {
+    expect(system.os).toBeUndefined();
+    expect(system.version).toBeDefined();
   });
 });
