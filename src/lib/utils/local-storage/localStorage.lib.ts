a3335712c7bdfe7fd4bb6b7f91f6133601bc79e3
EMMYbest1 <emmajerry2016@gmail.com>
2021-02-28 05:55:10 +0200

    Feature: type=test

--- src/lib/utils/local-storage/localStorage.lib.ts
+++ src/lib/utils/local-storage/localStorage.lib.ts
@@ -1,4 +1,4 @@
-/******************************************************************************************************************************************
+/************************************************************************************************************************************************
  * @docs this util is different from useLocalStorage because its built to be used in action e.g button click and not
  *  as a hook where its self invoked.
  * @useCase use it when you are trying to save to localStorage by action or when you want to save to localStorage in a function block.
@@ -8,7 +8,7 @@
  * save data to localStorage and give user the ability to pass in the key,vand their state name whihc they want the saved data to be stored
  * to and this util will simultaneously save to localStorage and if user gave the permission to refill their state, then we will refill their
  * state automatically.
- *******************************************************************************************************************************************/
+ *************************************************************************************************************************************************/
 
 const localStorage = (key?: string, stateDispatcher?: Function | ((newVal: any) => {})) => ({
   set(key: string, value: any): void {
--- src/lib/utils/local-storage/localStorage.test.tsx
+++ src/lib/utils/local-storage/localStorage.test.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {_localStorage} from './localStorage.lib';
 
-describe('Test for localStorage function', () => {
-  test('should...', () => {
-    expect(2 + 2).toBe(4);
+describe('Test for localStorage.lib.ts', () => {
+  afterAll(() => {
+    window.localStorage.clear();
+  });
+  test('should store and fetch primitive data successfully', () => {
+    _localStorage.set('test', 'test test test');
+
+    const getTest = _localStorage.get('test');
+
+    expect(getTest).toBeDefined();
+  });
+
+  test('should store and fetch reference data successfully', () => {
+    _localStorage.set('test2', {name: 'Emmanuel Onah', age: 21, occupation: 'Javascript Frontend Architect'});
+
+    const getTest = _localStorage.get('test2');
+
+    expect(getTest).toBeDefined();
+  });
+
+  test('should test for undefined value in localStorage and pass', () => {
+    const undefinedValue = _localStorage.get('test3') ?? undefined;
+
+    expect(undefinedValue).toBeUndefined();
   });
 });
